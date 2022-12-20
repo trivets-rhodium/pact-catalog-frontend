91ae12c115ebbb7480e0858fd39d677a43918a14
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-12-20 15:13:32 +0000

    feat(frontend): set readme as default tab with redirect

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,29 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   basePath: '/pact-catalog',
+  async redirects() {
+    return [
+      {
+        source: '/extensions/:slug*',
+        has: [
+          {
+            type: 'query',
+            key: '',
+            value: '',
+          },
+        ],
+        destination: '/extensions/:slug*',
+        has: [
+          {
+            type: 'query',
+            key: 'activeTab',
+            value: 'readme',
+          },
+        ],
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
