7fd07a5aebb73bdba3ad2084057f95d414d9f224
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-12-29 16:35:42 +0000

    feat(frontend): small style changes to search bar

--- frontend/components/search-bar.tsx
+++ frontend/components/search-bar.tsx
@@ -16,35 +16,45 @@ export default function SearchBar(props: SearchBarProps) {
   } = props;
   return (
     <div>
-      <select defaultValue="" name="searchType" onChange={onSearchTypeChange}>
-        <option disabled value="">
-          Type
-        </option>
-        <option value="dataModelExtensions">Data Model Extensions</option>
-        <option value="conformingSolutions">Conforming Solutions</option>
-      </select>
+      <h2 className="px-4">Search</h2>
+      <div className="flex justify-between mb-14 px-4">
+        <select
+          defaultValue=""
+          name="searchType"
+          onChange={onSearchTypeChange}
+          className="flex-grow m-1 p-4"
+        >
+          <option disabled value="">
+            Type
+          </option>
+          <option value="dataModelExtensions">Data Model Extensions</option>
+          <option value="conformingSolutions">Conforming Solutions</option>
+        </select>
 
-      <input
-        name="searchBar"
-        type="text"
-        onChange={onSearchValueChange}
-        placeholder="Title"
-      />
+        <input
+          name="searchBar"
+          type="text"
+          onChange={onSearchValueChange}
+          placeholder="Title"
+          className="flex-grow m-1"
+        />
 
-      <select
-        defaultValue="allPublishers"
-        name="publisher"
-        onChange={onPublisherChange}
-      >
-        <option value="allPublishers">All publishers</option>
-        {publishers.map((publisher) => {
-          return (
-            <option key={publisher} value={publisher}>
-              {publisher}
-            </option>
-          );
-        })}
-      </select>
+        <select
+          defaultValue="allPublishers"
+          name="publisher"
+          onChange={onPublisherChange}
+          className="flex-grow m-1"
+        >
+          <option value="allPublishers">All publishers</option>
+          {publishers.map((publisher) => {
+            return (
+              <option key={publisher} value={publisher}>
+                {publisher}
+              </option>
+            );
+          })}
+        </select>
+      </div>
     </div>
   );
 }
