d5901f45e5c496a41ffdc7ac0fb1015d3eeddcb3
Raimundo Henriques <raimundo.henriques@gmail.com>
2023-01-05 12:49:24 +0000

    config: set secret (test)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   basePath: '/pact-catalog',
-
+  env: {
+    secretKey: process.env.NEXT_PUBLIC_ACCESS_TOKEN,
+  },
 };
 
 module.exports = nextConfig;
