3efe262f6cd2fae0613a10bccde891069252acc1
Raimundo Henriques <raimundo.henriques@gmail.com>
2023-01-05 11:09:58 +0000

    refactor(frontend): create Card component and adapt Cards

--- frontend/components/cards.tsx
+++ frontend/components/cards.tsx
@@ -2,23 +2,15 @@ import { SearchResult } from 'minisearch';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import {
-  Key,
-  ReactElement,
-  JSXElementConstructor,
-  ReactFragment,
-  ReactPortal,
-} from 'react';
-import {
   CatalogDataModelExtension,
   CatalogUser,
   ConformingSolution,
   DMEId,
   VersionId,
 } from '../lib/catalog-types';
-
 import style from '../styles/Cards.module.css';
 
-export type CardsRenderer<T> = (cardDetails: T) => JSX.Element[];
+export type CardsRenderer<T> = (cardsContent: T) => JSX.Element[];
 
 type CardsProps<T> = {
   title: string;
@@ -42,16 +34,15 @@ export function Cards<T>(props: CardsProps<T>) {
       <ul className="grid grid-cols-4 m-1">
         {render(cardsContent)}
         {href && message && (
-          <Link href={href}>
-            <li
-              className={`${style['card-invert']} flex flex-col justify-between`}
-            >
-              <div>
-                <p className="text-xl font-bold">{message}</p>
-              </div>
-              <ul></ul>
-            </li>
-          </Link>
+          <Card
+            href={href}
+            title={message}
+            invert
+            cardContent={undefined}
+            render={(undefined) => {
+              return <></>;
+            }}
+          />
         )}
       </ul>
     </section>
@@ -59,69 +50,109 @@ export function Cards<T>(props: CardsProps<T>) {
 }
 
 export function extensionCards(
-  cardDetails: CatalogDataModelExtension[] | SearchResult[]
+  extensions: CatalogDataModelExtension[] | SearchResult[]
 ): JSX.Element[] {
-  return cardDetails.map(
-    ({ author, name, version, description, catalog_info, endorsers }) => {
-      return (
-        <Link
-          href={`/extensions/${name}/${version}`}
-          key={`${name}/${version}`}
-        >
-          <li className={`${style.card} flex flex-col justify-between`}>
-            <div>
-              <p className="font-bold">{description}</p>
-              <p>{version}</p>
-            </div>
-            <ul>
-              <li>Publisher: {author.name}</li>
-              <li>Status: {catalog_info.status}</li>
-              <li>
-                {endorsers.length ? `Endorsers (${endorsers.length}):` : ''}
-                <ul>
-                  {endorsers.map((endorser: CatalogUser) => {
-                    return <li key={endorser.id}>{endorser.name}</li>;
-                  })}
-                </ul>
-              </li>
-            </ul>
-          </li>
-        </Link>
-      );
-    }
+  return extensions.map((extension) => {
+    const { author, name, version, description, catalog_info, endorsers } =
+      extension;
+    return (
+      <Card
+        key={`${name}/${version}`}
+        href={`/extensions/${name}/${version}`}
+        title={description}
+        subtitle={version}
+        cardContent={extension}
+        render={renderExtensionCard}
+      />
+    );
+  });
+}
+
+function renderExtensionCard(
+  extension: CatalogDataModelExtension | SearchResult
+): JSX.Element {
+  const { author, catalog_info, endorsers } = extension;
+
+  return (
+    <ul>
+      <li>Publisher: {author.name}</li>
+      <li>Status: {catalog_info.status}</li>
+      <li>
+        {endorsers.length ? `Endorsers (${endorsers.length}):` : ''}
+        <ul>
+          {endorsers.map((endorser: CatalogUser) => {
+            return <li key={endorser.id}>{endorser.name}</li>;
+          })}
+        </ul>
+      </li>
+    </ul>
   );
 }
 
 export function solutionCards(
-  cardDetails: ConformingSolution[] | SearchResult[]
+  solutions: ConformingSolution[] | SearchResult[]
 ): JSX.Element[] {
-  return cardDetails.map(({ id, name, extensions, providerName }) => {
+  return solutions.map((solution) => {
+    const { id, name, extensions, providerName } = solution;
     return (
-      <Link href={`/solutions/${id}`} key={id}>
-        <li className={`${style.card} flex flex-col justify-between`}>
-          <div>
-            <p className="text-xl font-bold">{name}</p>
-            <p>{providerName}</p>
-          </div>
-          <div>
-            <ul>
-              {extensions.map(
-                (extension: {
-                  id: DMEId;
-                  version: VersionId;
-                  author: string;
-                }) => {
-                  return (
-                    <li key={extension.id}>
-                      {extension.id} {extension.version}
-                    </li>
-                  );
-                }
-              )}
-            </ul>
-          </div>
-        </li>
-      </Link>
+      <Card
+        key={id}
+        href={`/solutions/${id}`}
+        title={name}
+        subtitle={providerName}
+        cardContent={solution}
+        render={renderSolutionCard}
+      />
     );
   });
+}
+
+function renderSolutionCard(
+  solution: ConformingSolution | SearchResult
+): JSX.Element {
+  return (
+    <ul>
+      {solution.extensions.map(
+        (extension: { id: DMEId; version: VersionId; author: string }) => {
+          return (
+            <li key={extension.id}>
+              {extension.id} {extension.version}
+            </li>
+          );
+        }
+      )}
+    </ul>
+  );
+}
+
+type CardProps<T> = {
+  href: string;
+  key?: string;
+  title: string;
+  invert?: boolean;
+  subtitle?: string;
+  cardContent: T;
+  render: CardRenderer<T>;
+};
+
+export type CardRenderer<T> = (cardDetails: T) => JSX.Element;
+
+function Card<T>(props: CardProps<T>) {
+  const { href, key, title, invert, subtitle, cardContent, render } = props;
+
+  return (
+    <Link href={href} key={key}>
+      <li
+        className={`${
+          invert ? style['card-invert'] : style.card
+        } flex flex-col justify-between`}
+      >
+        <div>
+          <p className="font-bold">{title}</p>
+          <p>{subtitle}</p>
+        </div>
+        {render(cardContent)}
+      </li>
+    </Link>
+  );
 }
