1a75c46ef75c00023e1fe5f9e6692616dae2b2c7
Raimundo Henriques <raimundo.henriques@gmail.com>
2023-01-19 17:21:57 +0000

    style(app/api/auth/[...nextauth]): attempt to improve type safety; added comments

--- app/.next/static/chunks/webpack.js
+++ app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fd5c5df87cf9b4be"; }
+/******/ 		__webpack_require__.h = function() { return "06643cbda44d12ed"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app/pages/api/auth/[...nextauth].ts
+++ app/pages/api/auth/[...nextauth].ts
@@ -1,10 +1,18 @@
-import NextAuth, { NextAuthOptions } from 'next-auth';
+import NextAuth, {
+  Account,
+  Awaitable,
+  NextAuthOptions,
+  Profile,
+  Session,
+} from 'next-auth';
+import { JWT } from 'next-auth/jwt';
 import GithubProvider from 'next-auth/providers/github';
 import { getServerSideProps } from '../../auth/signin';
 
 export const authOptions = {
   providers: [
     GithubProvider({
+      // TO DO: improve error handling, in case environment variables are not provided?
       clientId: process.env.CLIENT_ID as string,
       clientSecret: process.env.CLIENT_SECRET as string,
       authorization: {
@@ -14,6 +22,7 @@ export const authOptions = {
     }),
   ],
   callbacks: {
+    // TO DO: improve type safety, although these are heavily dependent on NextAuth?
     async jwt({ token, account, profile }: any) {
       if (account) {
         token.accessToken = account.access_token;
