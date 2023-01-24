ca7a638f7dd9971fa1140188d0e9690702b0d498
Raimundo Henriques <raimundo.henriques@gmail.com>
2023-01-24 12:23:04 +0000

    chore(app): .next files

--- app/.next/static/chunks/webpack.js
+++ app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ddad2946271e2bcc"; }
+/******/ 		__webpack_require__.h = function() { return "3b53e410bc836ee3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
