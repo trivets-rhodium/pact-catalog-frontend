45c5d566cef1d2f92ecd4c4cf5b594012a3cf9bd
Raimundo Henriques <raimundo.henriques@gmail.com>
2023-01-24 18:14:19 +0000

    feat(catalog): two mock-data working-groups && feat(app): working-groups page

---
+++ app/pages/working-groups.tsx
@@ -0,0 +1,11 @@
+import Layout from '../components/layout';
+
+type PageProps = {};
+
+export default function WorkingGroups(props: PageProps) {
+  return (
+    <Layout>
+      <h1>Working Groups</h1>
+    </Layout>
+  );
+}
