00a1d00f9aa0db9b3b4d99f6d2cbd61b09fed4b2
Raimundo Henriques <raimundo.henriques@gmail.com>
2023-03-01 10:11:12 +0000

    feat: change landing page text and remove box-shadows

--- pages/index.tsx
+++ pages/index.tsx
@@ -66,7 +66,7 @@ export default function Home() {
                 className="link-background-3 hover:opacity-70"
                 href={'/collaborators'}
               >
-                collaborators
+                our collaborators
               </Link>
               .
             </p>
